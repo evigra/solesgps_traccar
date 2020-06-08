299203ca2c52782eacbddd8948820f9b8311dc5d
Eduardo Vizcaino <evigra@hotmail.com>
2020-06-08 11:52:34 -0500

    Update

--- solesgps_traccar/models/gpsmap.py
+++ solesgps_traccar/models/gpsmap.py
@@ -44,7 +44,7 @@ class vehicle(models.Model):
             fields_value    ="""\'%s\',""" %(vals["name"])
         if('imei' in vals):
             fields_name     ="""%suniqueid,""" %(fields_name)
-            fields_value    ="""%s\'%s\',""" %(fields_value,vals["imei"]),
+            fields_value    ="""%s%s,""" %(fields_value,vals["imei"]),
         if(fields_name!= False):
             sql='INSERT INTO tc_devices (%s) VALUES (%s)' %(fields_name,fields_value)
             print(sql)                     
