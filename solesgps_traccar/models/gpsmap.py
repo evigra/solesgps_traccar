9d61d5ed5e434d270606ec1c71def00f36d972a0
Eduardo Vizcaino <evigra@hotmail.com>
2020-06-08 22:26:52 -0500

    Update

--- solesgps_traccar/models/gpsmap.py
+++ solesgps_traccar/models/gpsmap.py
@@ -13,6 +13,17 @@ class tc_devices(models.Model):
     name            = fields.Char('Name', size=128)
     uniqueid        = fields.Char('Description', size=128)
 
+    def __SAVE(self,vals, method):
+        if('imei' in vals):
+            vals.uniqueid           =vals["imei"]
+            del vals["imei"]                   
+        return vals
+    def create(self,vals):
+        vals                        =self.__SAVE(vals)
+        return self.create(devices)    
+    def write(self,vals):
+        vals                        =self.__SAVE(vals)
+        return self.write(vals)    
 
 class positions(models.Model):
     _inherit = "gpsmap.positions"
@@ -26,32 +37,31 @@ class positions(models.Model):
 class vehicle(models.Model):
     _inherit = "fleet.vehicle"    
     def create(self,vals):
-        print('CREATE LALO====================')        
+        tc_devices_obj                  =self.env['tc_devices']        
+        devices                         ={}
 
-        #sql="INSERT INTO tc_devices (name,uniqueid,phone,disabled,speed) VALUES ('",vals["name"]"','",vals["uniqueid"],"','",vals["phone"],"','",vals["disabled"],"','",vals["speed"],"')"
-        #sql="INSERT INTO tc_devices (name,uniqueid,phone,speed) VALUES ('",vals["name"]"','",vals["uniqueid"],"','",vals["phone"],"','",vals["speed"],"')"
-        #sql="INSERT INTO tc_devices VALUES ('id','name','uniqueid','lastupdate','positionid','groupid','attributes','phone','model','contact','category','disabled','speed')"
-        #vals = {'name': 'ABC', 'standard':10}
-        #print(sql)        
+        if('name' in vals):
+            devices.name                =vals["name"]
+        if('imei' in vals):
+            devices.uniqueid            =vals["imei"]
+
+        tc_devices_obj.write(devices)    
+
         return super(vehicle, self).create(vals)
 
     def write(self,vals):
-        print('WRITE LALO====================')        
-
         tc_devices_obj                           =self.env['tc_devices']        
-
         devices_args                            =[('uniqueid','=',self.imei)]                
         devices_data                            =tc_devices_obj.search(devices_args, offset=0, limit=None, order=None)
-        print(devices_data)        
         if len(devices_data)>0:         
             for devices in devices_data:
-                print(devices)        
+                if('name' in vals):
+                    devices.name                =vals["name"]
+                if('imei' in vals):
+                    devices.uniqueid            =vals["imei"]
 
-        
-
-
-
-
-
-
+                tc_devices_obj.write(devices)    
+        else:
+            tc_devices_obj.create(devices)
         return super(vehicle, self).write(vals)
+
