f54529ae29eb9a8add4567050bd8a3ce53e849d3
Eduardo Vizcaino <evigra@hotmail.com>
2020-06-10 20:10:39 -0500

    Update

--- solesgps_traccar/models/gpsmap.py
+++ solesgps_traccar/models/gpsmap.py
@@ -28,7 +28,7 @@ class positions(models.Model):
     _inherit = "gpsmap.positions"
     
     def init(self):
-        self.env.cr.execute("""ALTER TABLE tc_positions ADD COLUMN IF !EXISTS read INTEGER""")
+        self.env.cr.execute("""ALTER TABLE IF EXISTS tc_positions ADD COLUMN IF NOT EXISTS leer INTEGER""")
 
     def run_scheduler_get_position2(self):
         self.env.cr.execute("""SELECT * FROM tc_positions ORDER BY id DESC""")
