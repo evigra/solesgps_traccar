545274d691e66ef951ede2e1c41dade1a252038a
Eduardo Vizcaino <evigra@hotmail.com>
2020-06-12 00:23:23 -0500

    Update

--- solesgps_traccar/models/gpsmap.py
+++ solesgps_traccar/models/gpsmap.py
@@ -33,7 +33,7 @@ class positions(models.Model):
         self.env.cr.execute("ALTER TABLE public.tc_positions ALTER COLUMN read SET NOT NULL;")
         self.env.cr.execute("ALTER TABLE public.tc_positions ALTER COLUMN read SET DEFAULT 0;")
     #"""
-    def run_scheduler_get_position2(self):
+    def run_scheduler_get_position(self):
         devices                     ={}
         self.env.cr.execute("""
             SELECT tp.protocol,fv.id as deviceid,tp.servertime,tp.devicetime,tp.fixtime,tp.valid,tp.latitude,tp.longitude,
@@ -46,12 +46,10 @@ class positions(models.Model):
         """)
         positions                   =self.env.cr.dictfetchall()
         
-        self.env.cr.execute("UPDATE tc_positions SET read=1 WHERE read=0")
-        #self.env.cr.dictfetchall()
-        
+        self.env.cr.execute("UPDATE tc_positions SET read=1 WHERE read=0")        
         for position in positions:
             self.create(position)
-                            
+        return super(positions, self).run_scheduler_get_position()                    
 class vehicle(models.Model):
     _inherit = "fleet.vehicle"    
 
