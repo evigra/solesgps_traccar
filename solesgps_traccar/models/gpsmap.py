cb326381372cef954f93e52b32894be1e7c7aaf9
Eduardo Vizcaino <evigra@hotmail.com>
2020-07-01 09:18:19 -0500

    Update

--- solesgps_traccar/models/gpsmap.py
+++ solesgps_traccar/models/gpsmap.py
@@ -61,7 +61,6 @@ class positions(models.Model):
 		            WHEN tp.attributes::json->>'alarm'!='' THEN 'alarm'
 		            WHEN tp.attributes::json->>'motion'='false' THEN 'deviceStopped'
 		            WHEN tp.attributes::json->>'motion'='true' THEN 'deviceOnline'
-		            ELSE te.type
 	            END	
                 as status,            
                 tp.protocol,fv.id as deviceid,tp.servertime,tp.devicetime,tp.fixtime,tp.valid,tp.latitude,tp.longitude,
@@ -69,7 +68,6 @@ class positions(models.Model):
             FROM tc_positions tp 
                 JOIN tc_devices td ON tp.deviceid=td.id 
                 JOIN fleet_vehicle fv ON fv.imei=td.uniqueid
-                LEFT JOIN tc_events te ON te.deviceid=td.id AND te.positionid=tp.id
             WHERE tp.read=0 
             ORDER BY tp.devicetime DESC 
         """)
