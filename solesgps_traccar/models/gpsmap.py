647d3d6d8427e4a29a74e09f68523bcfc08af32d
Eduardo Vizcaino <evigra@hotmail.com>
2020-07-01 08:09:11 -0500

    Update

--- solesgps_traccar/models/gpsmap.py
+++ solesgps_traccar/models/gpsmap.py
@@ -27,7 +27,7 @@ class tc_devices(models.Model):
 class positions(models.Model):
     _inherit = "gpsmap.positions"
     
-    #"""
+    """
     def init(self):
         self.env.cr.execute("ALTER TABLE public.tc_positions ADD COLUMN read integer;")
         self.env.cr.execute("ALTER TABLE public.tc_positions ALTER COLUMN read SET NOT NULL;")
